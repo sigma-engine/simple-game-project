9d0e09bfe93ed30e03cd73dfa54725d56ca94705
Aaron Siegel <siegelaaron94@gmail.com>
2018-12-02 14:24:25 -0800

    Initial project setup.

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,14 @@
+
+hunter_config(
+    glad
+    URL https://github.com/Dav1dde/glad/archive/v0.1.28.tar.gz
+    SHA1 10db58c9be03d6f3b223b6e19a14a034934b39a1
+    CMAKE_ARGS GLAD_INSTALL=ON
+)
+
+hunter_config(
+    debug-draw
+    URL https://github.com/sigma-engine/debug-draw/archive/v2.0.0.tar.gz
+    SHA1 c56130f893394972bac649a50eaa98c1f744886e
+    CMAKE_ARGS GLAD_INSTALL=ON
+)
